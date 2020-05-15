a99e3d0dac6d417900f8bff62380ac9a8cac53fd
Tyler Gibson <tgibson@microsoft.com>
2020-05-15 08:07:47 -0700

    Correcting Directory Structure for PSR-4 / PHP-PDS standards

--- inject-nomin.js
+++
@@ -1,3 +0,0 @@
-const insertLine = require('insert-line')
-
-insertLine('lib/mermaid.min.js').prependSync("/*@nomin*/")
---
+++ resources/utility/inject-nomin.js
@@ -0,0 +1,3 @@
+const insertLine = require('insert-line')
+
+insertLine('resources/mermaid.min.js').prependSync("/*@nomin*/")
