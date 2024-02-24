2b9bef9f9036c2a8b73a1be04fa102d127453620
marko220991 <19marko91.ilic@gmail.com>
2024-02-24 12:56:25 +0100

    Check and update assertions in tests

--- tests/phpunit/Unit/MermaidParserFunctionTest.php
+++ tests/phpunit/Unit/MermaidParserFunctionTest.php
@@ -66,7 +66,7 @@ class MermaidParserFunctionTest extends \PHPUnit_Framework_TestCase
 			$mockExtractor
 		);
 
-		$this->assertStringContainsString(
+		$this->assertContains(
 			$expected,
 			$instance->parse($text)
 		);
